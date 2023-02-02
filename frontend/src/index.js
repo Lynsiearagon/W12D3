8e6396e2d135bb17442652aafa48ccb965ede9ae
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-02-02 10:21:09 -0500

    Initial setup complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
+import configureStore from './store';
+
+
+const store = configureStore(); 
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store; 
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+// I believe the above would actually go in a different file on its own
+// and then you would input 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// Provider provides Redux Store to the Application 
+// Browser Router sets up your routes throughout your App 
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
 
 export const rootReducer = combineReducers({
-        // other reducers will go here later :>)
+    // other reducers will go here later :>)
 });
 
 let enhancer;
@@ -14,3 +15,19 @@ if (process.env.NODE_ENV === 'production') {
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
+
+const configureStore = (preloadedState) => {       // configureStore has createStore within it 
+    return createStore(rootReducer, preloadedState, enhancer);
+    // return createStore(rootReducer, preloadedState, applyMiddleware(thunk))
+}
+
+export default configureStore
+
+//    Things that go in your src/index.js file: 
+     
+//     - RootReducer 
+//         Takes in all the reducers 
+//     - Enhancer setup
+//         composeEnhancer(applyMiddleware(thunk, logger)) 
+//     - ConfigureStore, taking in preloaded state, returning 
+//         rootReducer, preloadedState, enhancer 
